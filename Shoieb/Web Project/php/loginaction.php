5283006f9e214878c6b27f1c4d1525e94d0b4b55
Rimon772 <39984119+Mosfak@users.noreply.github.com>
2019-10-19 20:17:44 +0600

    .

---
+++ Shoieb/Web Project/php/loginaction.php
@@ -0,0 +1,37 @@
+<?php
+  $servername="localhost";
+$username="root";
+$password="";
+$dbname="mydb";
+  
+$email=$_POST['email'];
+$pass=$_POST['pass'];
+
+
+$conn = new mysqli($servername,$username,$password,$dbname);
+
+if($conn->connect_error)
+{
+    echo "Connection Error!".$conn->connect_error;
+    die();
+}
+
+$sql="SELECT * FROM account WHERE email=? and pass=? ";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ss",$email,$pass);
+$stmt->execute();
+
+$stmt->store_result();
+
+if($stmt->num_rows>0){
+    echo "Login Successfull";
+    header("refresh:2;url='../Project.php");
+}
+else{
+    echo "Login Failed!!!!";
+           
+}
+
+
+?>
